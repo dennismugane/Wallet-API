860f1b23f8d1c5338c870fa58e9a7d2de0110dac
Dennis Muigo <dmuigo@gmail.com>
2026-04-30 13:10:11 +0300

    Fix circular dependency between SecurityConfig and JwtAuthFilter

--- src/main/java/com/muigo/wallet/security/JwtAuthFilter.java
+++ src/main/java/com/muigo/wallet/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Lazy;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +22,7 @@ import java.io.IOException;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    @Lazy
     private final UserDetailsService userDetailsService;
 
     @Override
